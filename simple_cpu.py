92325f6721953f178c5eb7b9f1dd73716dec1d89
SatyakiDe2019 <49108072+SatyakiDe2019@users.noreply.github.com>
2021-06-02 18:15:17 -0700

    Script using CPU to get it's performance

---
+++ simple_cpu.py
@@ -0,0 +1,29 @@
+##############################################
+#### Written By: SATYAKI DE               ####
+#### Written On: 18-Jan-2020              ####
+####                                      ####
+#### Objective: Main calling scripts for  ####
+#### normal execution.                    ####
+##############################################
+ 
+import numpy as np
+from timeit import default_timer as timer
+ 
+def pow(a, b, c):
+    for i in range(a.size):
+         c[i] = a[i] ** b[i]
+ 
+def main():
+    vec_size = 100000000
+ 
+    a = b = np.array(np.random.sample(vec_size), dtype=np.float32)
+    c = np.zeros(vec_size, dtype=np.float32)
+ 
+    start = timer()
+    pow(a, b, c)
+    duration = timer() - start
+ 
+    print(duration)
+ 
+if __name__ == '__main__':
+    main()
